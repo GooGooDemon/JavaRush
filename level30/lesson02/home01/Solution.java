7ea99d3d9331a93e51b01913a7f1fda39a7e6c2c
Nemchinov Sergey <googoodemon@gmail.com>
2017-03-21 13:13:23 +0700

    level30.lesson02

--- level30/lesson02/home01/Solution.java
+++ level30/lesson02/home01/Solution.java
@@ -1,5 +1,9 @@
 package com.javarush.test.level30.lesson02.home01;
 
+import java.math.BigInteger;
+
+import static com.javarush.test.level30.lesson02.home01.NumerationSystemType.*;
+
 /* Конвертер систем счислений
 Реализуйте логику метода convertNumberToOtherNumerationSystem, который должен переводить число number.getDigit()
 из одной системы счисления(numerationSystem) в другую (expectedNumerationSystem)
@@ -10,12 +14,36 @@ package com.javarush.test.level30.lesson02.home01;
 public class Solution {
     public static void main(String[] args) {
         Number number = new Number(NumerationSystemType._10, "6");
-        Number result = convertNumberToOtherNumerationSystem(number, NumerationSystemType._2);
+        Number result = convertNumberToOtherNumerationSystem(number, _2);
         System.out.println(result);    //expected 110
+
+        // exceptions expected
+        //System.out.println(convertNumberToOtherNumerationSystem(new Number(NumerationSystemType._10, "-120"), _2));
+        //System.out.println(convertNumberToOtherNumerationSystem(new Number(NumerationSystemType._10, "12.0"), _2));
+        //System.out.println(convertNumberToOtherNumerationSystem(new Number(NumerationSystemType._2, "120"), _10));
+        Number number1 = new Number(NumerationSystemType._2, "1101001000000001100001001110110111111100110010101000100111011011011001001011001100011001100000111101111");
+        System.out.println(convertNumberToOtherNumerationSystem(number1, _3));
+        System.out.println(convertNumberToOtherNumerationSystem(number1, _8));
+        System.out.println(convertNumberToOtherNumerationSystem(number1, _10)); //expected 519949509846173492335554907166
+        System.out.println(convertNumberToOtherNumerationSystem(number1, _16));
+
+        number = new Number(NumerationSystemType._10, "519949509846173492335554907166");
+        result = convertNumberToOtherNumerationSystem(number, NumerationSystemType._2);
+        System.out.println(result);    //expected 110100100000000110000100111011011111110011001010100010011101101101100100101100110001100110000011110
+
     }
 
     public static Number convertNumberToOtherNumerationSystem(Number number, NumerationSystem expectedNumerationSystem) {
+        if (!number.getDigit().matches("\\d*[a-fA-F]*"))
+        {
+            throw new NumberFormatException();
+        }
 
-        return null;
+        BigInteger bigInteger = new BigInteger(number.getDigit(), number.getNumerationSystem().getNumerationSystemIntValue());
+        String expectedValue = bigInteger.toString(expectedNumerationSystem.getNumerationSystemIntValue());
+
+        return new Number(expectedNumerationSystem, expectedValue);
     }
+
+
 }
--- level30/lesson02/task01/Solution.java
+++ level30/lesson02/task01/Solution.java
@@ -14,6 +14,18 @@ public class Solution {
     }
 
     public static String convertToDecimalSystem(String s) {
-        return s;
+        if (s.matches("0x\\d+"))
+        {
+            return String.valueOf(Integer.parseInt(s.substring(2), 16));
+        }
+        else if (s.matches("0b[0,1]+"))
+        {
+            return String.valueOf(Integer.parseInt(s.substring(2), 2));
+        }
+        else if (s.matches("0\\d+"))
+        {
+            return String.valueOf(Integer.parseInt(s, 8));
+        }
+        return String.valueOf(Integer.parseInt(s, 10));
     }
 }
